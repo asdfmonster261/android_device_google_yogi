bc79041e3c14602033e36910f891e57eed56dd96
Bob Dole <77771419+asdfmonster261@users.noreply.github.com>
2026-09-12 19:10:15 -0500

    yogi: Pull in the powerstats and wifi_ext policy
    We ship the power stats HAL and citadeld references its service type; the
    wifi.ext apex carries hal_wifi_ext_exec.
    
    Change-Id: Ibde0ba6d1f0285930169a02d29a3c68bd363f901

--- yogi/BoardConfig.mk
+++ yogi/BoardConfig.mk
@@ -88,9 +88,11 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy/vendor \
     hardware/google/pixel-sepolicy/citadel \
     hardware/google/pixel-sepolicy/power-libperfmgr \
+    hardware/google/pixel-sepolicy/powerstats \
     hardware/google/pixel-sepolicy/sscoredump \
     hardware/google/pixel-sepolicy/lineage_health \
     hardware/google/pixel-sepolicy/powershare \
-    hardware/google/pixel-sepolicy/touch
+    hardware/google/pixel-sepolicy/touch \
+    hardware/google/pixel-sepolicy/wifi_ext
 
 # TODO: include $(VENDOR_PATH)/BoardConfigVendor.mk once blobs are extracted
